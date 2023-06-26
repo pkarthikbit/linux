9bb46c3eba135f2a1a27ad35f0de89e8d0d87a9c
Karthik Palanisamy <pkarthikbit@gmail.com>
2023-06-26 08:17:42 +0530

    shell scrip to build and flash

---
+++ kpl_build.sh
@@ -0,0 +1,5 @@
+cd linux
+KERNEL=kernel8
+make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
+make menuconfig
+make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- Image modules dtbs
