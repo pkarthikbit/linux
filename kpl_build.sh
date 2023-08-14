b03eaffcbe2bd4e60c9b7f6f3abe8499177e185b
valliamenterprise <valliamenterprise@gmail.com>
2023-08-14 11:32:30 +0530

    [link] 1. change dir is not needed as the execution happens already inside linux dir 2. ref link updated

--- kpl_build.sh
+++ kpl_build.sh
@@ -1,4 +1,4 @@
-cd linux
+#https://www.raspberrypi.com/documentation/computers/linux_kernel.html#building-the-kernel-locally
 KERNEL=kernel8
 make ARCH=arm64 CROSS_COMPILE=aarch64-linux-gnu- bcm2711_defconfig
 make menuconfig
